de5740b002ddee85fc8aa2fff4c25dc7879c7040
Prasuna Dash <110610976+dprasuna@users.noreply.github.com>
2022-11-11 22:27:40 +0530

    Time: 15 ms (76.28%), Space: 44.4 MB (65.16%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        
+        if ( x==0 ){
+            return true;
+        }
+        
+        if (x<0 || x%10 == 0){
+            return false;
+        }
+        
+        int temp =x;
+        int rev =0;
+        
+        while( temp!=0 ){
+            int rem = temp%10;
+            temp = temp/10;
+            
+            rev = rev*10 + rem;
+        }
+        
+        if(x==rev){
+            return true;
+        }
+        else{
+            return false;
+        }
+        
+    }
+}
